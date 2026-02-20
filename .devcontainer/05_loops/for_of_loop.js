d76827ccbf5a2b5993957b7bcac6359f0332344b
BishnuPratap <bishnupratap85@gmail.com>
2026-02-20 07:21:39 +0000

    for of loop in js

---
+++ .devcontainer/05_loops/for_of_loop.js
@@ -0,0 +1,32 @@
+// for of loop
+
+// [" ", " ", " "]
+// [{ }, { }, { }]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+   // console.log(num)
+}
+
+const greetings = "Hello World!"
+for(const greet of  greetings) {
+   // console.log(`Ecah char is ${greet}`)
+
+}
+
+// MAPS
+
+const map = new Map() 
+
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('Fr', 'France')
+
+console.log(map);
+
+for (const key of map)
+    console.log(key)
+
+for (const [key, value] of map)
+    console.log(`${key} :- ${value}`);
