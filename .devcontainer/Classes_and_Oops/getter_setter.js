cca4749db722bc7aadb23d39cfea2a90fc044c4c
BishnuPratap <bishnupratap85@gmail.com>
2026-02-25 14:21:03 +0000

    getter anad setter in js

---
+++ .devcontainer/Classes_and_Oops/getter_setter.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this._password = password;  // store internally
+    }
+
+    get email(){
+        return this._email.toUpperCase();  // return with some formatting
+    }   
+    set email(value){
+        this._email = value;
+    }
+    
+    get password(){
+        return `${this._password}hitesh`;  // return with some formatting
+    }
+    
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("hitesh@gmail.com", "1236");
+console.log(hitesh.password); // "1236" in uppercase → "1236" (numbers stay same)
+console.log(hitesh.email); 
