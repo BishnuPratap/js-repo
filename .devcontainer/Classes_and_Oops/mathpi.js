d110891462bc06e9c27f106b58d5e0c310bd234b
BishnuPratap <bishnupratap85@gmail.com>
2026-02-25 12:58:10 +0000

    mathpi.js

---
+++ .devcontainer/Classes_and_Oops/mathpi.js
@@ -0,0 +1,28 @@
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isavailable: true
+};
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// Object.defineProperty(chai, "name", {
+//     value: "masala chai",
+//     writable: false,
+//     enumerable: true,
+//     configurable: true
+// });
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    enumerable: false
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== "function"){
+    console.log(key, value);
+    }
+}
